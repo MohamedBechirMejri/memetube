be2a8eb997ab93dd785e778c1b3b4e102eecbde0
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-07 22:04:27 +0100

    use dark logo

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ytLogoLight from "../Assets/Images/Logos/yt-logo-light.png";
+import ytLogoDark from "../Assets/Images/Logos/yt-logo-dark.png";
 
 function Sidebar(): JSX.Element {
   return (
     <div>
       <div>
         <span>|||</span>
-        <img src={ytLogoLight} alt="Youtube Logo" />
+        <img src={ytLogoDark} alt="Youtube Logo" />
       </div>
     </div>
   );
