a779eaea527cef1a70168fe92d94bd3f081398f9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-08 13:57:54 +0100

    position app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./Components/Sidebar";
 
 function App(): JSX.Element {
   return (
-    <div className="App">
+    <div className="bg-white dark:bg-[#1f232c] min-h-screen w-screen">
       <Sidebar />
     </div>
   );
