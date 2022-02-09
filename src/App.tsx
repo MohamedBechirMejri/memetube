81d889b7654eec545184654a5c59063806fbaf34
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-09 11:01:06 +0100

    add some states

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 
 function App(): JSX.Element {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <div className="bg-white dark:bg-[#1f232c] min-h-screen w-screen">
-      <Sidebar />
+      <Header
+        isDarkMode={isDarkMode}
+        setIsDarkMode={setIsDarkMode}
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />{" "}
+      <Sidebar isSidebarOpen={isSidebarOpen} />
     </div>
   );
 }
