07692a91240f676bd02d19d5db33f21f0a3b728c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-09 20:51:23 +0100

    put sidebar in div

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ function App(): JSX.Element {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
-    <div className="bg-white dark:bg-[#1f232c] min-h-screen w-screen">
+    <div className="bg-white dark:bg-[#1f232c] min-h-screen w-screen dark:text-white flex flex-col items-start justify-start">
       <Header
         isDarkMode={isDarkMode}
         setIsDarkMode={setIsDarkMode}
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />{" "}
-      <Sidebar isSidebarOpen={isSidebarOpen} />
+      <div>
+        <Sidebar isSidebarOpen={isSidebarOpen} />
+      </div>
     </div>
   );
 }
