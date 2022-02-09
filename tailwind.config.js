35b8d39027dc6d51b70ad559e08c02d2d8a509f0
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-09 10:59:31 +0100

    add reveal sidebar aniumtions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        revealSidebar: "revealSidebar 0.5s ease-in-out forwards",
+        hideSidebar: "hideSidebar 0.5s ease-in-out forwards",
+      },
+      keyframes: {
+        revealSidebar: {
+          "0%": {
+            width: "0",
+          },
+          "50%": {
+            width: "110%",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        hideSidebar: {
+          "0%": {
+            width: "110%",
+          },
+          "50%": {
+            width: "110%",
+          },
+          "100%": {
+            width: "0",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
