d7e61a8247ad5f14fab23a3e2e6e6e1cfa0c3647
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-09 22:23:33 +0100

    add searchbar

---
+++ src/Components/Header/Searchbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IoMicOutline } from "react-icons/io5";
+
+function Searchbar(): JSX.Element {
+  return (
+    <div className="flex items-center justify-start w-full h-full px-4 py-2 border-b-[.1px] border-l-[.1px] border-[#2c323a]">
+      <span className="text-2xl text-[#585e64] ">
+        <IoMicOutline />
+      </span>
+      <input
+        type="text"
+        className="text-[#585e64] bg-transparent outline-none p-4 placeholder:text-[#585e64]"
+        placeholder="Type to search"
+      />
+    </div>
+  );
+}
+
+export default Searchbar;
