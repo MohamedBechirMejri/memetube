bba6baff94cc7053053f4b71acd6f09793a141b1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-12 18:47:17 +0100

    add home component

---
+++ src/Components/Home.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+const videosList = [
+  {
+    id: 1,
+    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    channel: "youtube",
+    image: "https://img.youtube.com/vi/QH2-TGUlwu4/hqdefault.jpg",
+    views: "1,001,023",
+    likes: "1,001,023",
+    duration: "34:23",
+    video: "https://www.youtube.com/embed/QH2-TGUlwu4",
+  },
+  {
+    id: 2,
+    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    channel: "youtube",
+    image: "https://img.youtube.com/vi/QH2-TGUlwu4/hqdefault.jpg",
+    views: "1,001,023",
+    likes: "1,001,023",
+    duration: "34:23",
+    video: "https://www.youtube.com/embed/QH2-TGUlwu4",
+  },
+  {
+    id: 3,
+    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    channel: "youtube",
+    image: "https://img.youtube.com/vi/QH2-TGUlwu4/hqdefault.jpg",
+    views: "1,001,023",
+    likes: "1,001,023",
+    duration: "34:23",
+    video: "https://www.youtube.com/embed/QH2-TGUlwu4",
+  },
+  {
+    id: 4,
+    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    channel: "youtube",
+    image: "https://img.youtube.com/vi/QH2-TGUlwu4/hqdefault.jpg",
+    views: "1,001,023",
+    likes: "1,001,023",
+    duration: "34:23",
+    video: "https://www.youtube.com/embed/QH2-TGUlwu4",
+  },
+  {
+    id: 5,
+    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    channel: "youtube",
+    image: "https://img.youtube.com/vi/QH2-TGUlwu4/hqdefault.jpg",
+    views: "1,001,023",
+    likes: "1,001,023",
+    duration: "34:23",
+    video: "https://www.youtube.com/embed/QH2-TGUlwu4",
+  },
+  {
+    id: 6,
+    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    channel: "youtube",
+    image: "https://img.youtube.com/vi/QH2-TGUlwu4/hqdefault.jpg",
+    views: "1,001,023",
+    likes: "1,001,023",
+    duration: "34:23",
+    video: "https://www.youtube.com/embed/QH2-TGUlwu4",
+  },
+  {
+    id: 7,
+    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    channel: "youtube",
+    image: "https://img.youtube.com/vi/QH2-TGUlwu4/hqdefault.jpg",
+    views: "1,001,023",
+    likes: "1,001,023",
+    duration: "34:23",
+    video: "https://www.youtube.com/embed/QH2-TGUlwu4",
+  },
+];
+
+function Home(): JSX.Element {
+  return (
+    <div>
+      <div className="flex flex-wrap overflow-y-scroll">
+        {videosList.map(video => (
+          <div className="w-1/4 p-2">
+            <div className="p-4 bg-gray-800 rounded-lg shadow-lg">
+              <img src={video.image} alt={video.title} className="w-full" />
+              <div className="p-4">
+                <h3 className="mb-2 text-xl font-bold">{video.title}</h3>
+                <p className="text-sm text-gray-400">{video.channel}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
