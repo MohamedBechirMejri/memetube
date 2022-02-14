62883946d75851931e3b208eabb66049f2c8cf39
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-14 15:26:16 +0100

    import player component

--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Player from "./Video/Player";
 
 function Video(): JSX.Element {
   const { videoId } = useParams();
-  return <div>{videoId}</div>;
+  return (
+    <div>
+      <div>
+        <Player />
+      </div>
+      <div>{/* related videos */}</div>
+    </div>
+  );
 }
 
 export default Video;
