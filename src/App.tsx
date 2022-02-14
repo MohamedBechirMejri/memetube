02c00f1b72bac09359a9548b4eef74fe9cbf77d2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-14 15:17:42 +0100

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Sidebar from "./Components/Sidebar";
+import Video from "./Components/Video";
 
 function App(): JSX.Element {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="bg-white dark:bg-[#1f232c] min-h-screen w-screen dark:text-white flex flex-col items-start justify-start overflow-hidden">
       <Header
@@ -16,9 +19,17 @@ function App(): JSX.Element {
       />{" "}
       <div className="flex ">
         <Sidebar isSidebarOpen={isSidebarOpen} />
-        <Home />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/video/:videoId" element={<Video />} />
+        </Routes>
       </div>
     </div>
   );
 }
 export default App;
+//  <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="about" element={<About />} />
+//           <Route path="dashboard" element={<Dashboard />} />
