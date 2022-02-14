c662b68d05cf93563c901757150c752e02981ad5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-14 15:52:03 +0100

    add toolbox component

---
+++ src/Components/Video/ToolBox.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ToolBox(): JSX.Element {
+  return (
+    <div>
+      {" "}
+      <button type="button">Like</button>
+      <button type="button">Dislike</button>
+      <button type="button">Save</button>
+      <button type="button">Share</button>
+    </div>
+  );
+}
+
+export default ToolBox;
