e16dcff623e58aa3c93f78e6b415ccb5bd1ff463
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-14 16:23:42 +0100

    organize video page

--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Player from "./Video/Player";
 import RelatedVideos from "./Video/RelatedVideos";
+import ToolBox from "./Video/ToolBox";
+import Comments from "./Video/Comments";
+import Details from "./Video/Details";
 
 function Video(): JSX.Element {
   const { videoId } = useParams();
@@ -9,6 +12,17 @@ function Video(): JSX.Element {
     <div>
       <div>
         <Player />
+        <div>
+          <div>
+            <h1>video title</h1>
+            <p>views count</p>
+          </div>
+          <ToolBox />
+        </div>
+        <hr />
+        <Details />
+        <hr />
+        <Comments />
       </div>
       <RelatedVideos />
     </div>
