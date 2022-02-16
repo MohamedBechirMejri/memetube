cb4f04580ecf9eeb14d3eb885177a3a169f44187
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-16 17:25:34 +0100

    add test video

--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -11,7 +11,12 @@ function Video(): JSX.Element {
   return (
     <div>
       <div>
-        <Player />
+        <Player
+          url={
+            // TODO: get video url from backend
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          }
+        />
         <div>
           <div>
             <h1>video title</h1>
@@ -30,3 +35,31 @@ function Video(): JSX.Element {
 }
 
 export default Video;
+/**
+ * Test Videos
+const images = [
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerEscapes.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerFun.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerJoyrides.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/SubaruOutbackOnStreetAndDirt.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg"
+]
+
+const videos = [
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+	"https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+]
+ */
