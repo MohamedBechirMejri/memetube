1c91eaa9c29351a4a6dbc3172d53e92bdc3af5cb
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-16 17:48:37 +0100

    fix typo

--- src/Utils/addCommasToNumber.ts
+++ src/Utils/addCommasToNumber.ts
@@ -1,3 +1,4 @@
 const addCommasToNumber = (number: number): string =>
-  number.toString().replace(/\B(?=(\d{3})+(?!\d)/g, ",");
+  number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
 export default addCommasToNumber;
