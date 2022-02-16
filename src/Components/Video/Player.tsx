16c77e3274913a3b4ff70d9680ad0383ca08bbb9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-16 17:13:30 +0100

    add react player

--- src/Components/Video/Player.tsx
+++ src/Components/Video/Player.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 function Player(): JSX.Element {
-  return <div>{/*  */}</div>;
+  return (
+    <div>
+      <ReactPlayer
+        controls
+        url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+      />
+    </div>
+  );
 }
 
 export default Player;
