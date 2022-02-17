d72d9eb968c0e68bc12b7af8e14c2c20755170e9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-17 15:05:49 +0100

    style buttons

--- src/Components/Video/ToolBox.tsx
+++ src/Components/Video/ToolBox.tsx
@@ -1,13 +1,42 @@
 import React from "react";
+import {
+  AiFillDislike,
+  AiFillLike,
+  AiOutlineDislike,
+  AiOutlineLike,
+} from "react-icons/ai";
+import { MdOutlineLibraryAdd, MdOutlineShare } from "react-icons/md";
 
 function ToolBox(): JSX.Element {
   return (
-    <div>
+    <div className="flex items-center gap-2">
       {" "}
-      <button type="button">Like</button>
-      <button type="button">Dislike</button>
-      <button type="button">Save</button>
-      <button type="button">Share</button>
+      <button
+        type="button"
+        className="flex items-center gap-2 p-1 transition-all rounded hover:bg-slate-700 active:bg-slate-600"
+      >
+        {/* <AiFillLike /> */}
+        <AiOutlineLike />0
+      </button>
+      <button
+        type="button"
+        className="flex items-center gap-2 p-1 transition-all rounded hover:bg-slate-700 active:bg-slate-600"
+      >
+        {/* <AiFillDislike /> */}
+        <AiOutlineDislike />0
+      </button>
+      <button
+        type="button"
+        className="flex items-center gap-2 p-1 transition-all rounded hover:bg-slate-700 active:bg-slate-600"
+      >
+        <MdOutlineLibraryAdd /> Save
+      </button>
+      <button
+        type="button"
+        className="flex items-center gap-2 p-1 transition-all rounded hover:bg-slate-700 active:bg-slate-600"
+      >
+        <MdOutlineShare /> Share
+      </button>
     </div>
   );
 }
