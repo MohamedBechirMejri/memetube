61d4101abb383ad21c16974c7a7b3f29380aeee8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-27 07:12:57 +0100

    style addvideo modal

--- src/Components/Header/UserStuff.tsx
+++ src/Components/Header/UserStuff.tsx
@@ -8,6 +8,8 @@ function Userstuff(): JSX.Element {
   } = useContext(UserContext);
 
   const [isAddVideoShown, setIsAddVideoShown] = useState(true);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   return !user.name ? (
     <div className="flex items-center justify-center h-full gap-4 p-4 text-2xl">
@@ -16,10 +18,51 @@ function Userstuff(): JSX.Element {
           setIsAddVideoShown(!isAddVideoShown);
         }}
         className={` text-3xl transition-all cursor-pointer hover:scale-105 active:scale-95 ${
-          isAddVideoShown && "rotate-45"
+          isAddVideoShown && "rotate-[135deg]"
         } `}
       />
       {user.name}
+      {isAddVideoShown && (
+        <div className="fixed z-50 flex flex-col items-center justify-center gap-4 p-4   -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg dark:bg-[#1f232c] inset-1/2 w-fit h-fit ">
+          <label
+            htmlFor="title"
+            className="flex flex-col items-start justify-start"
+          >
+            Title
+            <input
+              type="text"
+              className="bg-transparent border rounded-lg"
+              id="title"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
+          </label>
+          <label
+            htmlFor="description"
+            className="flex flex-col items-start justify-start"
+          >
+            Description
+            <textarea
+              className="bg-transparent border rounded-lg"
+              id="description"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+            />
+          </label>{" "}
+          <label htmlFor="video-file">
+            <input type="file" />
+          </label>
+          <button
+            type="button"
+            className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700"
+            onClick={() => {
+              setIsAddVideoShown(!isAddVideoShown);
+            }}
+          >
+            Add Video
+          </button>
+        </div>
+      )}
     </div>
   ) : (
     <button
