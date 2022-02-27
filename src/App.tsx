d80a5229b275c3a0d5f082855d6b94a4f4f7edb1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-27 04:56:08 +0100

    add user state

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,40 @@ import Home from "./Components/Home";
 import Sidebar from "./Components/Sidebar";
 import User from "./Components/User";
 import Video from "./Components/Video";
+import UserContext from "./Utils/UserContext";
 
 function App(): JSX.Element {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isAddVideoShown, setIsAddVideoShown] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+  });
 
   return (
-    <div className="bg-white dark:bg-[#1f232c] min-h-screen w-screen dark:text-white flex flex-col items-start justify-start overflow-hidden">
-      <Header
-        isDarkMode={isDarkMode}
-        setIsDarkMode={setIsDarkMode}
-        isSidebarOpen={isSidebarOpen}
-        setIsSidebarOpen={setIsSidebarOpen}
-      />{" "}
-      <div className="flex w-full">
-        <Sidebar isSidebarOpen={isSidebarOpen} />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/video/:videoId" element={<Video />} />
-          <Route path="/user/:userId" element={<User />} />
-        </Routes>
+    <UserContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={user}
+    >
+      <div className="bg-white dark:bg-[#1f232c] min-h-screen w-screen dark:text-white flex flex-col items-start justify-start overflow-hidden">
+        <Header
+          isDarkMode={isDarkMode}
+          setIsDarkMode={setIsDarkMode}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+          isAddVideoShown={isAddVideoShown}
+          setIsAddVideoShown={setIsAddVideoShown}
+        />{" "}
+        <div className="flex w-full">
+          <Sidebar isSidebarOpen={isSidebarOpen} />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/video/:videoId" element={<Video />} />
+            <Route path="/user/:userId" element={<User />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </UserContext.Provider>
   );
 }
 export default App;
