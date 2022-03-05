a7b346f7264d61c9a17e3316cb48484395e83773
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-03-05 03:26:37 +0100

    add video data type

---
+++ src/Types/VideoData.ts
@@ -0,0 +1,17 @@
+type VideoData = {
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+  uploader: {
+    displayName: string;
+    photoURL: string;
+    id: string;
+  };
+  likes: string[];
+  dislikes: string[];
+  comments: object[];
+  views: number;
+  date: string;
+};
+export default VideoData;
