f08bdb9750e9ea8f91aef383ee93516a8882b2f5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-02-09 02:23:51 +0100

    disable ts-cceck

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 /**
  * This file is included in `/next.config.mjs` which ensures the app isn't built with invalid env vars.
  * It has to be a `.mjs`-file to be imported there.
@@ -11,7 +10,7 @@ const _serverEnv = serverSchema.safeParse(serverEnv);
 if (!_serverEnv.success) {
   console.error(
     "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
+    ...formatErrors(_serverEnv.error.format())
   );
   throw new Error("Invalid environment variables");
 }
