4397ab505fda8ee673aeaaddf2c554a1d80bd482
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-02-11 07:00:22 +0100

    delete old code

--- src.old/Components/Video/RelatedVideos.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function RelatedVideos(): JSX.Element {
-  return <div />;
-}
-
-export default RelatedVideos;
--- src.old/Utils/addCommasToNumber.ts
+++
@@ -1,4 +0,0 @@
-const addCommasToNumber = (number: number): string =>
-  number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-export default addCommasToNumber;
