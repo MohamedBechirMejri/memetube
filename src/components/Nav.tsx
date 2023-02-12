d61df85658089d551cb418323d640f98b88f0a55
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-02-12 07:58:43 +0100

    add nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,9 +6,13 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineLogout, AiOutlineUser } from "react-icons/ai";
+import { FaRegBookmark } from "react-icons/fa";
+import { useState } from "react";
 
 const Nav = ({ user, signIn }: { user: any; signIn: any }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="absolute z-50 flex h-[5rem] w-full items-center justify-between px-8 text-xl">
       <Link href="/">
@@ -24,17 +28,49 @@ const Nav = ({ user, signIn }: { user: any; signIn: any }) => {
           </button>
         </Link>
         {user ? (
-          <Link href={`/u/${user.uid}`}>
+          <div className="relative">
             <Image
               width={44}
               height={44}
               alt="user"
-              className="relative z-10 h-11 w-11 rounded-full text-3xl transition-all hover:elevation-2"
+              className="relative z-10 h-11 w-11 cursor-pointer rounded-full text-3xl transition-all hover:elevation-2"
               // @ts-ignore
               src={user.photoURL}
               draggable={false}
+              onClick={() => setIsMenuOpen((prev) => !prev)}
             />
-          </Link>
+
+            {isMenuOpen && (
+              <ul
+                className="absolute right-full top-full grid-rows-3 overflow-hidden rounded-2xl bg-white elevation-4  group-focus:grid"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <li>
+                  <Link
+                    href={`/u/${user.uid}`}
+                    className="grid grid-cols-[2rem,4.5rem] place-items-center gap-2 p-2 px-4 transition-all hover:text-green-500 "
+                  >
+                    <AiOutlineUser />
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/saved"
+                    className="grid grid-cols-[2rem,4.5rem] place-items-center gap-2 p-2 px-4 transition-all hover:text-blue-500 "
+                  >
+                    <FaRegBookmark /> Saved
+                  </Link>
+                </li>
+                <li>
+                  <button className="grid grid-cols-[2rem,4.5rem] place-items-center gap-2 p-2 px-4 transition-all hover:text-red-500 ">
+                    <AiOutlineLogout />
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            )}
+          </div>
         ) : (
           <button
             className="grid h-10 w-10 place-items-center rounded-full border border-current text-3xl text-fuchsia-500"
