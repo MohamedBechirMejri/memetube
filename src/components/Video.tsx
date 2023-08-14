96700c59cff2e88977cb1a4b645294415ba82664
BASSEM <bassem.jadoui@esprit.tn>
2023-08-14 18:30:58 +0100

    configure and apply prettier

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,55 +3,55 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import { useInView } from "react-intersection-observer";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useInView } from 'react-intersection-observer'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 const Video = ({ src }: { src: string }) => {
-  const ref = useRef();
-  const bgRef = useRef();
-  const { ref: inViewRef, inView } = useInView({threshold: 0.75});
-  const [isPlaying, setIsPlaying] = useState(false);
+    const ref = useRef()
+    const bgRef = useRef()
+    const { ref: inViewRef, inView } = useInView({ threshold: 0.75 })
+    const [isPlaying, setIsPlaying] = useState(false)
 
-  const setRefs = useCallback(
-    (node: any) => {
-      ref.current = node;
-      inViewRef(node);
-    },
-    [inViewRef]
-  );
+    const setRefs = useCallback(
+        (node: any) => {
+            ref.current = node
+            inViewRef(node)
+        },
+        [inViewRef],
+    )
 
-  useEffect(() => {
-    // @ts-ignore
-    isPlaying ? bgRef.current.play() : bgRef.current.pause();
-  }, [isPlaying]);
+    useEffect(() => {
+        // @ts-ignore
+        isPlaying ? bgRef.current.play() : bgRef.current.pause()
+    }, [isPlaying])
 
-  useEffect(() => {
-    // @ts-ignore
-    inView ? ref.current.play() : ref.current.pause();
-  }, [inView, ref]);
+    useEffect(() => {
+        // @ts-ignore
+        inView ? ref.current.play() : ref.current.pause()
+    }, [inView, ref])
 
-  return (
-    <div className="relative flex h-full items-center justify-center overflow-hidden rounded-3xl bg-black elevation-12">
-      <video
-        ref={bgRef}
-        muted
-        loop
-        playsInline
-        src={src}
-        className="absolute h-full w-full object-fill blur-3xl"
-      />
-      <video
-        ref={setRefs}
-        src={src}
-        controls
-        loop
-        playsInline
-        onPlay={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
-        className="relative z-10 h-full elevation-8"
-      />
-    </div>
-  );
-};
+    return (
+        <div className="relative flex h-full items-center justify-center overflow-hidden rounded-3xl bg-black elevation-12">
+            <video
+                ref={bgRef}
+                muted
+                loop
+                playsInline
+                src={src}
+                className="absolute h-full w-full object-fill blur-3xl"
+            />
+            <video
+                ref={setRefs}
+                src={src}
+                controls
+                loop
+                playsInline
+                onPlay={() => setIsPlaying(true)}
+                onPause={() => setIsPlaying(false)}
+                className="relative z-10 h-full elevation-8"
+            />
+        </div>
+    )
+}
 
-export default Video;
+export default Video
