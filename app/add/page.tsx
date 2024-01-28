c751ab45e0b0b2fbc0e66690b37baa72bb112529
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-28 18:20:42 +0100

    Add page component with upload functionality

---
+++ app/add/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { UploadButton } from "~/utils/uploadthing";
+
+export default function Add() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </main>
+  );
+}
