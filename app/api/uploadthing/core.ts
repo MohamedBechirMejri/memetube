15b7c3fd77b234b06ded5d3641acdc020f351578
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-28 18:26:30 +0100

    Update maxFileSize for video uploads

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -7,7 +7,7 @@ const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({ video: { maxFileSize: "64MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
