406bc262054b48b42f6b6a7707b0e054db5688f0
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-28 19:21:18 +0100

    Update auth function to use "mbm" as the user ID

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -2,7 +2,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+const auth = (req: Request) => ({ id: "mbm" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
