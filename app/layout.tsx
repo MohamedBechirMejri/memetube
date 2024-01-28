f9f7c9508abb7b89832ab5c2ef9076b1f71c505f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-28 21:28:25 +0100

    Remove unused import statement

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import '~/lib/firebase'
 
 const inter = Inter({ subsets: ["latin"] });
 
