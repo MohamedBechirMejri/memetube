08cdb3ae4b46c1f50109bbf7802387e1773a2bff
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-28 22:05:13 +0100

    Refactor Reel component to use destructured video properties

--- app/(home)/Reel.tsx
+++ app/(home)/Reel.tsx
@@ -2,10 +2,22 @@ import { motion, useInView } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 type Props = {
-  video: string;
+  video: {
+    key: string;
+    likes: number;
+    name: string;
+    serverData: {
+      uploadedBy: string;
+    };
+    size: number;
+    title: string;
+    url: string;
+  };
 };
 
 export default function Reel({ video }: Props) {
+  const { name, title, url } = video;
+
   const ref = useRef<HTMLDivElement>(null);
   const isInView = useInView(ref, { amount: 0.5 });
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -18,11 +30,10 @@ export default function Reel({ video }: Props) {
   return (
     <div
       ref={ref}
-      key={video.url}
-      className="relative flex h-[calc(100svh-5rem)] snap-center items-center mb-8 w-full"
+      className="relative mb-8 flex h-[calc(100svh-5rem)] w-full snap-center items-center"
     >
-      <video ref={videoRef} src={video} className="max-h-full max-w-full" />
-      <p className="absolute bottom-0 left-4">video name.mp4</p>
+      <video ref={videoRef} src={url} className="max-h-full max-w-full" />
+      <p className="absolute bottom-0 left-4">{title || name}</p>
     </div>
   );
 }
