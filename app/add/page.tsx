2f67c62fb0a76dc796b1237c5bb556bcd0792ba4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-28 20:28:34 +0100

    Add video upload functionality

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,8 +1,40 @@
 "use client";
 
+import { child, get, getDatabase, ref, set } from "firebase/database";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
 import { UploadDropzone } from "~/utils/uploadthing";
 
 export default function Add() {
+  const [title, setTitle] = useState("");
+  const router = useRouter();
+
+  const addVideo = useCallback(
+    (res: any) => {
+      const dbRef = ref(getDatabase());
+      get(child(dbRef, `videos/`))
+        .then(async (snapshot) => {
+          if (snapshot.exists()) {
+            const id = new Crypto().randomUUID();
+            const db = await getDatabase();
+
+            await set(ref(db, "videos/" + id), {
+              title,
+              likes: 0,
+              comments: [],
+              ...res,
+            });
+          }
+        })
+        .then(() => {
+          setTitle("");
+          router.push("/");
+        })
+        .catch((error) => console.error(error));
+    },
+    [router, title],
+  );
+
   return (
     <main className="flex h-full flex-col items-center justify-center p-4">
       <h1 className="p-2 text-2xl capitalize">Upload a new meme</h1>
@@ -13,18 +45,13 @@ export default function Add() {
         type="text"
         placeholder="something relevant"
         className="mb-4 w-full rounded-xl border border-slate-700 bg-transparent p-4 py-2 text-xl outline-none"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <UploadDropzone
         endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
+        onClientUploadComplete={(res) => addVideo(res)}
+        onUploadError={(error: Error) => alert(`ERROR! ${error.message}`)}
         className="h-full w-full rounded-lg bg-slate-950 p-4 text-white"
       />
     </main>
