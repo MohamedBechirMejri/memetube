1370332d27a71c011fef1872eb486ef304969281
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-28 19:02:15 +0100

    Add title input field and update main container styles

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -4,7 +4,16 @@ import { UploadDropzone } from "~/utils/uploadthing";
 
 export default function Add() {
   return (
-    <main className="flex h-full flex-col items-center justify-center">
+    <main className="flex h-full flex-col items-center justify-center p-4">
+      <h1 className="p-2 text-2xl capitalize">Upload a new meme</h1>
+      <label className="mb-3 mt-4 w-full px-2 text-left capitalize">
+        Add a title:
+      </label>
+      <input
+        type="text"
+        placeholder="something relevant"
+        className="mb-4 w-full rounded-xl border border-slate-700 bg-transparent p-4 text-xl outline-none"
+      />
       <UploadDropzone
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
