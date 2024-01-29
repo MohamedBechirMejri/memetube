d660911653db624f642a2e61a2825315a6b5ebb4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-01-29 21:40:12 +0100

    add blurred bg

--- app/(home)/Reel.tsx
+++ app/(home)/Reel.tsx
@@ -21,10 +21,16 @@ export default function Reel({ video }: Props) {
   const ref = useRef<HTMLDivElement>(null);
   const isInView = useInView(ref, { amount: 0.5 });
   const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef2 = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    if (isInView) videoRef.current?.play();
-    else videoRef.current?.pause();
+    if (isInView) {
+      videoRef.current?.play();
+      videoRef2.current?.play();
+    } else {
+      videoRef.current?.pause();
+      videoRef2.current?.pause();
+    }
   }, [isInView]);
 
   return (
@@ -32,7 +38,17 @@ export default function Reel({ video }: Props) {
       ref={ref}
       className="relative mb-8 flex h-[calc(100svh-5rem)] w-full snap-center items-center"
     >
-      <video ref={videoRef} src={url} className="max-h-full max-w-full w-full" />
+      <video
+        ref={videoRef2}
+        src={url}
+        className="absolute inset-0 w-full h-full scale-[2] blur-2xl brightness-50"
+        muted
+      />
+      <video
+        ref={videoRef}
+        src={url}
+        className="relative z-10 max-h-full w-full max-w-full"
+      />
       {/* <p className="absolute bottom-0 left-4">{title || name}</p> */}
     </div>
   );
