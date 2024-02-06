13269fdcc81143254931e7d3421510e7ad2e5d8f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-02-06 02:22:30 +0100

    Add search and profile links to navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import { TbBrandGoogleHome, TbPlus, TbStar } from "react-icons/tb";
+import { TbBrandGoogleHome, TbPlus, TbSearch, TbStar, TbUser } from "react-icons/tb";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,10 +27,14 @@ export default function RootLayout({
       >
         <main className="grid h-[100svh] w-full max-w-[38rem] grid-rows-[minmax(0,1fr),auto] bg-black">
           {children}
-          <nav className="relative z-50 grid h-20 w-full grid-cols-3 place-items-center">
+          <nav className="relative z-50 grid h-20 w-full grid-cols-5 place-items-center">
             <Link href={"/"} className="flex flex-col items-center ">
               <TbBrandGoogleHome />
               Home
+            </Link>
+            <Link href={"/search"} className="flex flex-col items-center ">
+              <TbSearch />
+              Search
             </Link>
             <Link
               href="/add"
@@ -41,6 +45,10 @@ export default function RootLayout({
             <Link href="/favorites" className="flex flex-col items-center ">
               <TbStar />
               Favorites
+            </Link>
+            <Link href={"/profile"} className="flex flex-col items-center ">
+              <TbUser />
+              Search
             </Link>
           </nav>
         </main>
