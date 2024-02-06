d1ecae27052e22923eb2494fba6a634b393df807
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-02-06 02:32:12 +0100

    Add video creation date to Reel component

--- app/(home)/Reel.tsx
+++ app/(home)/Reel.tsx
@@ -1,5 +1,5 @@
 import { motion, useInView } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 type Props = {
   video: {
@@ -12,36 +12,38 @@ type Props = {
     size: number;
     title: string;
     url: string;
+    createdAt: number;
   };
 };
 
 export default function Reel({ video }: Props) {
-  const { name, title, url } = video;
+  const { name, title, url, createdAt } = video;
 
   const ref = useRef<HTMLDivElement>(null);
   const isInView = useInView(ref, { amount: 0.5 });
   const videoRef = useRef<HTMLVideoElement>(null);
   const videoRef2 = useRef<HTMLVideoElement>(null);
+  const [status, setStatus] = useState("stopped");
 
   useEffect(() => {
-    if (isInView) {
-      videoRef.current?.play();
-      videoRef2.current?.play();
-    } else {
-      videoRef.current?.pause();
-      videoRef2.current?.pause();
-    }
+    if (isInView) setStatus("playing");
+    else setStatus("stopped");
   }, [isInView]);
 
-  const togglePlay = () => {
-    if (videoRef.current?.paused) {
-      videoRef.current?.play();
-      videoRef2.current?.play();
-    } else {
+  const togglePlay = useCallback(() => {
+    if (status === "playing") setStatus("stopped");
+    else setStatus("playing");
+  }, [status]);
+
+  useEffect(() => {
+    if (status === "stopped") {
       videoRef.current?.pause();
       videoRef2.current?.pause();
+    } else if (status === "playing") {
+      videoRef.current?.play();
+      videoRef2.current?.play();
     }
-  };
+  }, [status]);
 
   return (
     <div
@@ -61,6 +63,9 @@ export default function Reel({ video }: Props) {
         className="relative z-10 max-h-full w-full max-w-full"
       />
       {/* <p className="absolute bottom-0 left-4">{title || name}</p> */}
+      <p className="absolute bottom-4 left-4 z-50">
+        {new Date(createdAt).toDateString()}
+      </p>
     </div>
   );
 }
