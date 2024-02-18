6e573bf3b73420c0ef3311cd62d2545cb3209806
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-02-18 09:43:14 +0100

    Add Firebase integration and user signal handling

--- lib/signals/user.ts
+++ lib/signals/user.ts
@@ -1,3 +1,22 @@
-import { signal } from "@preact/signals-react"; 
+import { effect, signal } from "@preact/signals-react";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "../firebase";
 
-export const userSig = signal<any>({});
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export const userSig = signal<any>(null);
+export const UID = signal<string | null>(auth.currentUser?.uid || null);
+
+effect(() => {
+  if (UID.value)
+    getDoc(doc(db, "users", UID.value)).then((doc) => {
+      if (doc.exists()) {
+        userSig.value = doc.data();
+      }
+    });
+  else userSig.value = null;
+});
