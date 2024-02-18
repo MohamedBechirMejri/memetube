94ab4565f6085f0f3518872574eabf44e191efb4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-02-18 11:00:43 +0100

    Remove unused Firebase imports and console.log statement

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { firebaseConfig } from "~/lib/firebase";
 import { userSig } from "~/lib/signals/user";
 
 export default function Profile() {
-  // const app = initializeApp(firebaseConfig);
-  // console.log(getAuth(app));
-  console.log(userSig.value);
-
   return (
     <main className="flex h-full flex-col items-center justify-center p-4">
       Soon...
