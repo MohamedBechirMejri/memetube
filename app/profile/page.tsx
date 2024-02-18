01bf0157389458360628a855936946521da3fd68
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-02-18 08:45:10 +0100

    Add userSig console.log in Profile component

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { userSig } from "~/lib/signals/user";
+
 export default function Profile() {
+  console.log(userSig.value);
+
   return (
     <main className="flex h-full flex-col items-center justify-center p-4">
       Soon...
