cb49d53bacf0be709e03d6a469ac03314ffae8fd
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-02-18 09:43:03 +0100

    Imported and commented out Firebase dependencies and initialization code

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { firebaseConfig } from "~/lib/firebase";
 import { userSig } from "~/lib/signals/user";
 
 export default function Profile() {
+  // const app = initializeApp(firebaseConfig);
+  // console.log(getAuth(app));
   console.log(userSig.value);
 
   return (
