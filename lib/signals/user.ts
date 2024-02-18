a50c36210027bdcefef52690fcaf6eb6f8c30b48
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-02-18 08:29:34 +0100

    Update user.ts to fix authSig initialization

--- lib/signals/user.ts
+++ lib/signals/user.ts
@@ -2,5 +2,4 @@ import { signal } from "@preact/signals-react";
 import { getAuth } from "firebase/auth";
 
 export const userSig = signal<any>({});
-
-export const authSig = signal(getAuth());
+export const authSig = signal<any>(null);
