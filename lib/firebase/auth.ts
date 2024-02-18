94007012d172b75d33bde736095f99a8f5e43147
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-02-18 08:29:24 +0100

    Refactor signOut function

--- lib/firebase/auth.ts
+++ lib/firebase/auth.ts
@@ -1,31 +1,7 @@
-import { doc, getFirestore, setDoc } from "firebase/firestore";
-import { authSig, userSig } from "../signals/user";
-import {
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithPopup,
-} from "firebase/auth";
+import { authSig } from "../signals/user";
 
 const auth = authSig.value;
 
-onAuthStateChanged(auth, (u) => (userSig.value = u));
-
-export const signIn = async () => {
-  const db = getFirestore();
-
-  const result = await signInWithPopup(auth, new GoogleAuthProvider());
-  const userData = result.user;
-  console.log(userData);
-
-  await setDoc(
-    doc(db, "users", userData.uid),
-    {
-      displayName: userData.displayName,
-      photoURL: userData.photoURL,
-      uid: userData.uid,
-    },
-    { merge: true },
-  );
+export const signOut = () => {
+  if (auth) auth.signOut();
 };
-
-export const signOut = () => auth.signOut();
