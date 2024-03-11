38e6b1a35e19103bc046eb73fe62cef53dc2e587
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-11 23:07:25 +0100

    Add user name to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,9 +3,12 @@
 import { userSig } from "~/lib/globals/user";
 
 export default function Profile() {
+  const user = userSig.value;
+
   return (
     <main className="flex h-full flex-col items-center justify-center p-4">
       Soon...
+      {!!user && user.name}
     </main>
   );
 }
