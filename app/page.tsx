3d34177b58406b8301f993993ea9a2d7094d6d19
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-12 01:28:20 +0100

    Add user state to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { firebaseConfig, getCollection } from "~/lib/firebase";
 import Reel from "./(home)/Reel";
+import { useUserStore } from "~/lib/globals/user";
 
 export default function Home() {
   const [videos, setVideos] = useState<any[]>([]);
@@ -13,18 +14,24 @@ export default function Home() {
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
 
+  const { user } = useUserStore();
+
   useEffect(() => {
+    console.log(user);
     getCollection(db, "videos").then((querySnapshot) => {
       const videos = querySnapshot.docs.map((doc) => doc.data());
       setVideos(videos);
     });
-  }, [db]);
+  }, [db, user]);
 
   return (
     <main className="h-full w-full snap-y snap-mandatory overflow-y-scroll">
-      {videos.map((video) => (
+      {/* {videos.map((video) => (
         <Reel key={video.id} video={video} />
-      ))}
+      ))} */}
+      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        WIP...
+      </div>
     </main>
   );
 }
