aa82d6f32a3cea72ed0f6a05f9c3b137503b28f8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-12 01:23:13 +0100

    Refactor video fetching in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { get, getDatabase, ref } from "firebase/database";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -10,36 +9,21 @@ import Reel from "./(home)/Reel";
 
 export default function Home() {
   const [videos, setVideos] = useState<any[]>([]);
-  const videosSnapshotRef = useRef(null);
 
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
 
-  getCollection(db, "videos").then((querySnapshot) => {
-    const videos = querySnapshot.docs.map((doc) => doc.data());
-    // setVideos(videos);
-    console.log(videos);
-  });
-
   useEffect(() => {
-    const db = getDatabase();
-    const videosRef = ref(db, "videos");
-    get(videosRef)
-      .then((snapshot) => {
-        const data = snapshot.val();
-        if (!data) return;
-        const videos = Object.values(data).sort(
-          (a: any, b: any) => b.createdAt - a.createdAt,
-        );
-        setVideos(videos);
-      })
-      .catch((error) => console.error(error));
-  }, []);
+    getCollection(db, "videos").then((querySnapshot) => {
+      const videos = querySnapshot.docs.map((doc) => doc.data());
+      setVideos(videos);
+    });
+  }, [db]);
 
   return (
     <main className="h-full w-full snap-y snap-mandatory overflow-y-scroll">
       {videos.map((video) => (
-        <Reel key={video.key} video={video} />
+        <Reel key={video.id} video={video} />
       ))}
     </main>
   );
