42e85ca03556879af487620c9ed9ee7edbc07579
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-12 22:02:49 +0100

    Update function to handle edge cases

---
+++ types/User.ts
@@ -0,0 +1,14 @@
+export type User = {
+  uid: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+  image: string;
+  name: string;
+  favorites: string[];
+  history: string[];
+  likes: string[];
+  uploads: string[];
+  createdAt: number;
+  updatedAt: number;
+};
