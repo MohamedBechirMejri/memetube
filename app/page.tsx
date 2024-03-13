6bca06474c6401cd9be4c79367df3fb216033e68
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-13 01:41:32 +0100

    Remove commented out code and update video rendering

--- app/page.tsx
+++ app/page.tsx
@@ -26,12 +26,9 @@ export default function Home() {
 
   return (
     <main className="h-full w-full snap-y snap-mandatory overflow-y-scroll">
-      {/* {videos.map((video) => (
+      {videos.map((video) => (
         <Reel key={video.id} video={video} />
-      ))} */}
-      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        WIP...
-      </div>
+      ))}
     </main>
   );
 }
