f3d48c7123c2609c554571de36632a409e03957c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-19 12:19:36 +0100

    chore: Add user authentication redirect in Profile component

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { useUserStore } from "~/lib/globals/user";
 
 export default function Profile() {
   const { user } = useUserStore();
 
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) router.push("/login");
+  }, [router, user]);
+
   return (
     <main className="flex h-full flex-col items-center justify-center p-4">
       Soon...
-      {!!user && user.name}
+      {user?.name}
     </main>
   );
 }
