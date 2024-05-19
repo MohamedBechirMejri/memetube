307711e10f13444828115d8e831b5f4f80a5d101
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-19 12:19:32 +0100

    chore: Refactor Login component to include lazy loading for images

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,17 +14,15 @@ import { useEffect } from "react";
 import { firebaseConfig } from "~/lib/firebase";
 import { useUserStore } from "~/lib/globals/user";
 import { User } from "~/types/User";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const db = getFirestore(app);
+  const router = useRouter();
 
-  const { setUID } = useUserStore();
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (u) => setUID(u?.uid || null));
-  }, [auth, setUID]);
+  const { setUID, uid } = useUserStore();
 
   const signIn = async () => {
     await setPersistence(auth, browserLocalPersistence);
@@ -67,10 +65,21 @@ export default function Login() {
     );
   };
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (u) => setUID(u?.uid || null));
+  }, [auth, setUID]);
+
+  useEffect(() => {
+    if (uid) router.back();
+  }, [router, uid]);
+
   return (
     <main className="flex h-full flex-col items-center justify-center p-4">
-      Soon...
-      <button onClick={signIn}>login</button>
+      <h1 className="text-xl font-bold text-rose-500">Login to continue</h1>
+
+      <button onClick={signIn} className="mt-4 rounded border p-2 px-8">
+        Sign in with Google
+      </button>
     </main>
   );
 }
