6f50039f59b9080012db3a046e339f8aaed6bb94
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-20 17:56:14 +0100

    refactor: Sort videos by createdAt in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -22,15 +22,18 @@ export default function Home() {
       const videos = querySnapshot.docs.map((doc) => doc.data());
       setVideos(videos);
     });
-  }, [db, user]);
+  }, []);
 
   return (
     <main className="h-full w-full snap-y snap-mandatory overflow-hidden overflow-y-scroll">
       {videos
         .filter((v) => {
           return true;
-          // if (!user) return true;
+          if (!user) return true;
           // return !user.history.includes("videos/" + v.id);
+        })
+        .sort((a, b) => {
+          return b.createdAt - a.createdAt;
         })
         .map((video) => (
           <Reel key={video.id} video={video} />
