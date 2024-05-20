7b35c30fdecbd741f72997b25869a38d641184b6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-20 10:32:42 +0100

    chore: Update Profile component to include Firebase authentication and logout button

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { initializeApp } from "firebase/app";
+import { getAuth, signOut } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { firebaseConfig } from "~/lib/firebase";
 import { useUserStore } from "~/lib/globals/user";
 
 export default function Profile() {
   const { user } = useUserStore();
 
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
   const router = useRouter();
 
   useEffect(() => {
@@ -19,7 +24,10 @@ export default function Profile() {
         Hello, {user?.name.split(" ")[0]}
       </h1>
 
-      <button className="rounded-2xl bg-rose-500 bg-opacity-15 p-2 px-8 text-rose-500">
+      <button
+        className="rounded-2xl bg-rose-500 bg-opacity-15 p-2 px-8 text-rose-500"
+        onClick={() => signOut(auth)}
+      >
         Logout
       </button>
     </main>
