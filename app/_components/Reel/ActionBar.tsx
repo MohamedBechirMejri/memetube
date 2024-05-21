4f9eac8d36d0c09ca2896e4e5cbb24395da24d5e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-21 08:11:01 +0100

    refactor: Update ActionBar component UI and functionality

--- app/_components/Reel/ActionBar.tsx
+++ app/_components/Reel/ActionBar.tsx
@@ -1,7 +1,7 @@
 import { FaCommentDots } from "react-icons/fa";
 import { IoIosShareAlt } from "react-icons/io";
 import { IoHeart } from "react-icons/io5";
-import { RxBookmarkFilled } from "react-icons/rx";
+import { RxBookmarkFilled, RxSpeakerOff } from "react-icons/rx";
 import { TbDots } from "react-icons/tb";
 import { Video } from "~/types/Video";
 
@@ -10,25 +10,54 @@ type Props = {
 };
 
 export default function ActionBar({ video }: Props) {
+  const buttons = [
+    {
+      icon: IoHeart,
+      text: video.likes.length,
+      action: () => {},
+      isActive: false,
+    },
+    {
+      icon: FaCommentDots,
+      text: video.comments.length,
+      action: () => {},
+      isActive: false,
+    },
+    {
+      icon: RxBookmarkFilled,
+      text: "",
+      action: () => {},
+      isActive: false,
+    },
+    {
+      icon: IoIosShareAlt,
+      text: "",
+      action: () => {},
+      isActive: false,
+    },
+  ];
+
   return (
-    <div className="absolute bottom-0 right-0 z-30 flex flex-col gap-4 pb-4">
+    <div className="absolute bottom-0 right-0 z-30 flex h-full flex-col justify-between gap-4">
       <button className="p-4 text-3xl">
-        <TbDots />
+        <RxSpeakerOff />
       </button>
-      <button className="p-4">
-        <IoHeart className="text-3xl" />
-        {video.likes.length}
-      </button>
-      <button className="p-4">
-        <FaCommentDots className="text-3xl" />
-        {video.comments.length}
-      </button>
-      <button className="p-4">
-        <RxBookmarkFilled className="text-3xl" />
-      </button>
-      <button className="p-4">
-        <IoIosShareAlt className="text-3xl" />
-      </button>
+
+      <div className="flex flex-col gap-2">
+        <button className="p-4 text-3xl">
+          <TbDots />
+        </button>
+        {buttons.map((button, i) => (
+          <button
+            key={i}
+            onClick={button.action}
+            className="flex flex-col items-center gap-1 p-4"
+          >
+            {<button.icon className="text-3xl" />}
+            <span>{button.text}</span>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
