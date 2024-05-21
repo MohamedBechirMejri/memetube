b093d0d60475f54e89648633b5a4f7fbea1f851d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-21 14:42:04 +0100

    refactor: Add lazy loading for comment author images in Comment component

--- app/_components/Reel/ActionBar/Comments/Comment.tsx
+++ app/_components/Reel/ActionBar/Comments/Comment.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { FaHeart } from "react-icons/fa";
 
 import type { Comment as CommentT } from "~/types/Video";
@@ -8,15 +9,29 @@ type Props = {
 
 export default function Comment({ comment }: Props) {
   return (
-    <div key={comment.createdAt} className="flex justify-between gap-4 p-4">
-      <div className="flex flex-col">
-        <span className="font-semibold">{comment.author.name}</span>
-        <span>{comment.body}</span>
+    <div
+      key={comment.createdAt}
+      className="flex items-start justify-between gap-4 p-4"
+    >
+      <div className="flex items-start gap-2">
+        <Image
+          src={comment.author.image}
+          alt={comment.author.name}
+          className="size-10 rounded-full object-contain"
+          width={40}
+          height={40}
+        />
+        <div className="flex flex-col gap-1">
+          <span className="font-semibold opacity-70">
+            {comment.author.name}
+          </span>
+          <p className="text-balance">{comment.body}</p>
+        </div>
       </div>
 
-      <button className="flex items-center gap-1">
+      <button className="flex shrink-0 flex-col items-center gap-1 text-xl">
         <FaHeart />
-        <span>{comment.likes.length}</span>
+        <span className="text-xs">{comment.likes.length}</span>
       </button>
     </div>
   );
