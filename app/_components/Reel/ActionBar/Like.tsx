0e24cd56fcdf378a0b1896d39c5eb4d46cde76f6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-21 10:56:54 +0100

    refactor: Update Like component to handle user likes and dislikes

--- app/_components/Reel/ActionBar/Like.tsx
+++ app/_components/Reel/ActionBar/Like.tsx
@@ -1,15 +1,35 @@
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { IoHeart } from "react-icons/io5";
+import { useUserStore } from "~/lib/globals/user";
 import { useVideoStore } from "~/lib/globals/video";
 
 export default function Like() {
   const { video } = useVideoStore();
+  const { user, setUser } = useUserStore();
 
-  const handleLike = () => {};
+  const db = getFirestore();
+
+  const handleLike = async () => {
+    if (!user || !video) return;
+
+    let likes;
+
+    if (video.likes.includes("users/" + user.uid)) {
+      likes = video.likes.filter((like) => like !== "users/" + user.uid);
+    } else {
+      likes = [...video.likes, "users/" + user.uid];
+    }
+
+    await setDoc(doc(db, "videos", video.id), { likes }, { merge: true });
+  };
 
   return (
     <button
       onClick={handleLike}
-      className="flex flex-col items-center gap-1 p-4"
+      className={
+        "flex flex-col items-center gap-1 p-4" +
+        (video?.likes.includes("users/" + user?.uid) ? " text-red-500" : "")
+      }
     >
       {<IoHeart className="text-3xl" />}
       <span>{video?.likes.length}</span>
