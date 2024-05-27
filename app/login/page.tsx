8f9083e01497d8bb87774c6ec63240265c9c4851
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-27 02:44:10 +0100

    refactor: Update login page to use signInWithRedirect for Google sign-in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,19 +5,16 @@ import {
   GoogleAuthProvider,
   browserLocalPersistence,
   getAuth,
-  getRedirectResult,
   onAuthStateChanged,
   setPersistence,
-  // signInWithPopup,
-  signInWithRedirect,
+  signInWithPopup,
 } from "firebase/auth";
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { Button } from "~/components/ui/button";
 import { firebaseConfig } from "~/lib/firebase";
 import { useUserStore } from "~/lib/globals/user";
 import { User } from "~/types/User";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
   const app = initializeApp(firebaseConfig);
@@ -29,72 +26,58 @@ export default function Login() {
 
   const signIn = async () => {
     await setPersistence(auth, browserLocalPersistence);
-    await signInWithRedirect(auth, new GoogleAuthProvider());
+
+    const result = await signInWithPopup(auth, new GoogleAuthProvider());
+    const { email, displayName, photoURL, uid } = result.user;
+
+    if (!uid || !email || !displayName || !photoURL) return;
+
+    const currentData = await getDoc(doc(db, "users", uid));
+
+    const now = Date.now();
+
+    const stubData = {
+      favorites: [],
+      history: [],
+      likes: [],
+      uploads: [],
+      createdAt: now,
+      preferences: {
+        language: "any",
+        nsfw: false,
+      },
+    } as {
+      favorites: string[];
+      history: string[];
+      likes: string[];
+      uploads: string[];
+      createdAt: number;
+      preferences: {
+        language: "arabic" | "english" | "any";
+        nsfw: boolean;
+      };
+    };
+
+    const data = currentData.exists() ? (currentData.data() as User) : stubData;
+
+    const u: User = {
+      ...data,
+      email,
+      name: displayName,
+      image: photoURL,
+      uid,
+      displayName,
+      photoURL,
+      updatedAt: now,
+    };
+
+    await setDoc(doc(db, "users", uid), u, { merge: true });
   };
 
   useEffect(() => {
-    const handleRedirect = async () => {
-      const result = (await getRedirectResult(auth)) as any;
+    const unsub = onAuthStateChanged(auth, (u) => setUID(u?.uid || null));
 
-      if (!result) return;
-
-      const { email, displayName, photoURL, uid } = result.user;
-
-      if (!uid || !email || !displayName || !photoURL) return;
-
-      const currentData = await getDoc(doc(db, "users", uid));
-
-      const now = Date.now();
-
-      const stubData = {
-        favorites: [],
-        history: [],
-        likes: [],
-        uploads: [],
-        createdAt: now,
-        preferences: {
-          language: "any",
-          nsfw: false,
-        },
-      } as {
-        favorites: string[];
-        history: string[];
-        likes: string[];
-        uploads: string[];
-        createdAt: number;
-        preferences: {
-          language: "arabic" | "english" | "any";
-          nsfw: boolean;
-        };
-      };
-
-      const data = currentData.exists()
-        ? (currentData.data() as User)
-        : stubData;
-
-      const u: User = {
-        ...data,
-        email,
-        name: displayName,
-        image: photoURL,
-        uid,
-        displayName,
-        photoURL,
-        updatedAt: now,
-      };
-
-      await setDoc(doc(db, "users", uid), u, { merge: true });
-
-      router.push("/");
-    };
-
-    handleRedirect();
-  }, [auth, db, router]);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (u) => setUID(u?.uid || null));
-
-    return () => unsubscribe();
+    return () => unsub();
   }, [auth, setUID]);
 
   useEffect(() => {
@@ -105,9 +88,9 @@ export default function Login() {
     <main className="flex h-full flex-col items-center justify-center p-4">
       <h1 className="text-xl font-bold text-rose-500">Login to continue</h1>
 
-      <Button onClick={signIn} className="mt-4 rounded border p-2 px-8">
+      <button onClick={signIn} className="mt-4 rounded border p-2 px-8">
         Sign in with Google
-      </Button>
+      </button>
     </main>
   );
 }
