9eb4987002a2c5b1c26b65a7d90bf54f399ff951
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-27 01:07:58 +0100

    feat: Add History component to profile page
    The profile page has been updated to include a History component. This change allows users to view their watch history directly from the profile page. It enhances the user experience by providing a convenient and centralized location for accessing their viewing history.

---
+++ app/profile/History.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Reel from "~/components/Reel";
+import ActionBar from "~/components/Reel/ActionBar";
+import { useUserStore } from "~/lib/globals/user";
+import { useVideoStore } from "~/lib/globals/video";
+
+export default function History() {
+  const { video, collection } = useVideoStore();
+  const { user } = useUserStore();
+
+  const sortedVideos = (collection || [])
+    .filter((v) => user?.history.includes(v.id))
+    .sort((a, b) => b.createdAt - a.createdAt);
+
+  return (
+    <main className="h-full w-full snap-y snap-mandatory overflow-hidden overflow-y-scroll">
+      <ActionBar />
+
+      {video && (
+        <p className="absolute bottom-16 left-0 z-30 w-full bg-gradient-to-t from-slate-950 p-4 font-semibold">
+          <span className="line-clamp-1 w-[65%]">{video.name}</span>
+          <span className="items-center text-sm font-normal opacity-80">
+            {new Date(video.createdAt).toDateString()} <br />
+            {video.views.length} views
+          </span>
+        </p>
+      )}
+
+      {sortedVideos.map((video, i) => (
+        <Reel key={video.id} video={video} i={i} />
+      ))}
+    </main>
+  );
+}
