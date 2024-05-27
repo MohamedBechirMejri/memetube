4b67cbde358da94e92ca351764f7ba15599da119
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-27 04:03:30 +0100

    refactor: Update Nav component to use dynamic links
    The Nav component has been refactored to use dynamic links instead of hardcoding them. This change improves code maintainability and allows for easier addition or modification of navigation links in the future.

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -11,52 +11,34 @@ import {
 import Image from "next/image";
 import { useUserStore } from "~/lib/globals/user";
 
+const links = [
+  { href: "/", name: "Home", icon: <TbBrandGoogleHome /> },
+  { href: "/search", name: "Search", icon: <TbSearch /> },
+  { href: "/add", name: "", icon: <TbPlus /> },
+  { href: "/favorites", name: "Favorites", icon: <TbStar /> },
+  { href: "/profile", name: "Profile", icon: <TbUser /> },
+];
+
 export default function Nav() {
   const { user } = useUserStore();
-  
+
   return (
-    <nav className="relative z-50 grid h-16 w-full grid-cols-5 grid-rows-1 place-items-center ">
-      <Link href={"/"} className="flex flex-col items-center justify-between ">
-        <TbBrandGoogleHome />
-        Home
-      </Link>
-      <Link
-        href={"/search"}
-        className="flex flex-col items-center justify-between gap-1 "
-      >
-        <TbSearch />
-        Search
-      </Link>
-      <Link
-        href="/add"
-        className="-mt-2 flex flex-col items-center rounded-full bg-white p-2 px-6 text-black"
-      >
-        <TbPlus className="text-3xl" />
-      </Link>
-      <Link
-        href="/favorites"
-        className="flex flex-col items-center justify-between "
-      >
-        <TbStar />
-        Favorites
-      </Link>
-      <Link
-        href={"/profile"}
-        className="flex flex-col items-center justify-between "
-      >
-        {user ? (
-          <Image
-            src={user.image}
-            width={48}
-            height={48}
-            alt="user"
-            className="h-6 w-6 rounded-full"
-          />
-        ) : (
-          <TbUser />
-        )}
-        Profile
-      </Link>
+    <nav className="relative z-50 grid h-16 w-full grid-cols-5 grid-rows-1">
+      {links.map((link) => (
+        <Link
+          href={link.href}
+          key={link.href}
+          className={
+            "flex flex-col items-center justify-center gap-1 text-xl" +
+            (link.href === "/add"
+              ? " ghosting-text h-12 rounded-2xl bg-white bg-opacity-40"
+              : "")
+          }
+        >
+          {link.icon}
+          <span className="text-xs">{link.name}</span>
+        </Link>
+      ))}
     </nav>
   );
 }
