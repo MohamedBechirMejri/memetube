72f2db4758966412bc7c9943e123c0f60583652b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-27 04:33:20 +0100

    ```text refactor: Update layout component to use LayoutClient and remove unused code
    The layout component has been refactored to use the LayoutClient component instead of the previous implementation. This change improves code organization and removes unused code, resulting in a cleaner and more efficient layout.

---
+++ app/LayoutClient.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { firebaseConfig } from "~/lib/firebase";
+import Nav from "./Nav";
+import { useUserStore } from "~/lib/globals/user";
+import { useEffect } from "react";
+import { collection, doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { usePathname } from "next/navigation";
+import { Video } from "~/types/Video";
+import { useVideoStore } from "~/lib/globals/video";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const analytics = getAnalytics(app);
+
+  const { setUID, setUser, uid, user } = useUserStore();
+  const { setCollection } = useVideoStore();
+
+  const pathname = usePathname();
+
+  let page;
+
+  switch (pathname) {
+    case "/add":
+      page = "New Meme";
+      break;
+    case "/search":
+      page = "Search Memes";
+      break;
+    case "/profile":
+      page = user ? "Hello, " + user.name.split(" ")[0] : "Profile";
+      break;
+    default:
+      page = "MemeStash";
+  }
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (u) => setUID(u?.uid || null));
+
+    auth.authStateReady().then(() => setUID(auth.currentUser?.uid || null));
+  }, [auth, setUID]);
+
+  useEffect(() => {
+    if (uid)
+      onSnapshot(doc(db, "users", uid), (doc) => {
+        if (doc.exists()) setUser(doc.data());
+        else {
+          setUser(null);
+          signOut(auth);
+        }
+      });
+    else setUser(null);
+  }, [auth, db, setUser, uid]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "videos"),
+      (snapshot) => {
+        const videos = snapshot.docs.map((doc) => doc.data()) as Video[];
+        setCollection(videos);
+      },
+      (error) => console.error(error),
+    );
+
+    return () => unsubscribe();
+  }, [db, setCollection]);
+
+  return (
+    <div className="relative grid h-[100svh] w-full max-w-[38rem] grid-rows-[minmax(0,1fr),auto]">
+      <h1 className="ghosting-text absolute z-50 w-full py-4 text-center text-2xl font-bold">
+        {page}
+      </h1>
+      {children}
+      <Nav />
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,107 +1,68 @@
-"use client";
-
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { Inter } from "next/font/google";
-import { firebaseConfig } from "~/lib/firebase";
-import Nav from "./Nav";
+import Layout from "./LayoutClient";
 import "./globals.scss";
-import { useUserStore } from "~/lib/globals/user";
-import { useEffect } from "react";
-import { collection, doc, getFirestore, onSnapshot } from "firebase/firestore";
-import { usePathname } from "next/navigation";
-import { Video } from "~/types/Video";
-import { useVideoStore } from "~/lib/globals/video";
+import { Metadata, Viewport } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// const analytics = getAnalytics(app);
+const APP_NAME = "MemeStash";
+const APP_DEFAULT_TITLE = "MemeStash";
+const APP_TITLE_TEMPLATE = "%s - MemeStash";
+const APP_DESCRIPTION = "Collection of the best memes around the web";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#020617",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
-
-  const { setUID, setUser, uid, user } = useUserStore();
-  const { setCollection } = useVideoStore();
-
-  const pathname = usePathname();
-
-  let page;
-
-  switch (pathname) {
-    case "/add":
-      page = "New Meme";
-      break;
-    case "/search":
-      page = "Search Memes";
-      break;
-    case "/profile":
-      page = user ? "Hello, " + user.name.split(" ")[0] : "Profile";
-      break;
-    default:
-      page = "MemeStash";
-  }
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (u) => setUID(u?.uid || null));
-
-    auth.authStateReady().then(() => setUID(auth.currentUser?.uid || null));
-  }, [auth, setUID]);
-
-  useEffect(() => {
-    if (uid)
-      onSnapshot(doc(db, "users", uid), (doc) => {
-        if (doc.exists()) setUser(doc.data());
-        else {
-          setUser(null);
-          signOut(auth);
-        }
-      });
-    else setUser(null);
-  }, [auth, db, setUser, uid]);
-
-  useEffect(() => {
-    const unsubscribe = onSnapshot(
-      collection(db, "videos"),
-      (snapshot) => {
-        const videos = snapshot.docs.map((doc) => doc.data()) as Video[];
-        setCollection(videos);
-      },
-      (error) => console.error(error),
-    );
-
-    return () => unsubscribe();
-  }, [db, setCollection]);
-
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>MemeStash</title>
-        <meta
-          name="description"
-          content="Collection of the best memes around the web"
-        />
-      </head>
       <body
         className={
           inter.className +
           " flex h-[100svh] w-[100svw] flex-col items-center justify-between bg-slate-950 text-white antialiased"
         }
       >
-        <div className="relative grid h-[100svh] w-full max-w-[38rem] grid-rows-[minmax(0,1fr),auto]">
-          <h1 className="ghosting-text absolute z-50 w-full py-4 text-center text-2xl font-bold">
-            {page}
-          </h1>
-          {children}
-          <Nav />
-        </div>
+        <Layout>{children}</Layout>
       </body>
     </html>
   );
