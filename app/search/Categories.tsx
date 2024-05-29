79e67021911eebe8635ed3a821d8c78073293729
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-29 05:52:51 +0100

    refactor: Add framer-motion animation to Categories page
    The code changes in the app/search/Categories.tsx file refactor the Categories page by adding framer-motion animation. The category links now have an initial fade-in animation when they are rendered, a scale animation on hover, and a smooth transition animation when clicked. This change enhances the visual appeal and user experience of the Categories page.

--- app/search/Categories.tsx
+++ app/search/Categories.tsx
@@ -1,10 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore, onSnapshot } from "firebase/firestore";
 import Image from "next/image";
-import Link from "next/link";
+import L from "next/link";
 import { useEffect, useState } from "react";
 import { firebaseConfig } from "~/lib/firebase";
 import { Category } from "~/types/Video";
+import { motion } from "framer-motion";
+
+const Link = motion(L);
 
 export default function Categories() {
   const [categories, setCategories] = useState<Category[]>([]);
@@ -27,11 +30,23 @@ export default function Categories() {
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      {categories.map((category) => (
+      {categories.map((category, i) => (
         <Link
+          initial={{ opacity: 0, scale: 0.9, y: 20 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 1 }}
+          transition={{
+            type: "spring",
+            damping: 10,
+            stiffness: 100,
+            duration: 0.3,
+            delay: i > 6 ? 0 : i * 0.1,
+            scale: { type: "spring", damping: 10, stiffness: 100, delay: 0 },
+          }}
           href={`/search/c/${category.id}`}
           key={category.id}
-          className="relative overflow-hidden rounded-lg ring-rose-500 ring-opacity-20 transition-all duration-300 hover:ring"
+          className="relative overflow-hidden rounded-lg ring-rose-500 ring-opacity-20 hover:ring"
         >
           <h2 className="ghosting-text absolute flex h-full w-full items-center justify-center bg-black bg-opacity-20 text-xl font-semibold text-gray-300">
             {category.name}
