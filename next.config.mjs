ae267e9c4bd76920dac3f9c1e4e656d02cbac93a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-29 03:22:58 +0100

    disable millionlint

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,33 @@
-import MillionLint from '@million/lint';
+// import MillionLint from "@million/lint";
 import withSerwistInit from "@serwist/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    reactCompiler: process.env.NODE_ENV === "development" ? false : true
+    reactCompiler: process.env.NODE_ENV === "development" ? true : true,
     // ppr: true,
   },
   // allow all image domains
   images: {
-    remotePatterns: [{
-      protocol: "https",
-      hostname: "**"
-    }]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
   // react 19 breaks framer motion so this is a workaround until it gets fixed
-  reactStrictMode: process.env.NODE_ENV === "development" ? false : true
+  reactStrictMode: process.env.NODE_ENV === "development" ? false : true,
 };
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
   disable: process.env.NODE_ENV === "development",
   // exclude manifest.json from service worker
-  exclude: [/^manifest/]
+  exclude: [/^manifest/],
 });
-export default MillionLint.next({
-  rsc: true
-})(withSerwist(nextConfig));
+// export default MillionLint.next({
+//   rsc: true
+// })(withSerwist(nextConfig));
+
+export default withSerwist(nextConfig);
