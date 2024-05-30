0c797568839d45c835437e249275fca0630cb3ae
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-05-30 01:37:09 +0100

    refactor: Optimize lazy loading of videos in Reel component
    The code changes in the components/Reel/index.tsx file optimize the lazy loading of videos in the Reel component. The previous implementation had a condition that was not correctly evaluating whether the videos should be rendered. This caused unnecessary re-fetching of videos and decreased performance. The condition has been fixed to properly check if the index is greater than or equal to the current video index or the next two video indexes. This change improves the efficiency and rendering of videos in the Reel component.

--- components/Reel/index.tsx
+++ components/Reel/index.tsx
@@ -77,14 +77,14 @@ export default function Reel({ video, i = 0 }: Props) {
   }, [i, isInView, setIndex, setVideo, video]);
 
   // allowing all previous videos to render so we don't have to re-fetch them and preparing the next 2 videos to render
-  const canRender = index || 0 >= i || index === i + 1 || index === i + 2;
+  // const canRender = index || 0 >= i || index === i + 1 || index === i + 2;
 
   return (
     <div
       ref={ref}
       className="relative mb-8 flex h-full w-screen max-w-[38rem] snap-center items-center"
     >
-      {canRender && <VideoPlayer url={url} isInView={isInView} />}
+      <VideoPlayer url={url} isInView={isInView} />
     </div>
   );
 }
